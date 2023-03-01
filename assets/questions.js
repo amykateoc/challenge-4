8547af62d89d2e074130baa59f83e525622e79dc
Amy Kate <amykateoc6@gmail.com>
2023-03-01 17:33:36 -0500

    adding files to repo

---
+++ assets/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        title: "this is question 1",
+        options: ["option1", "options2", "option3"],
+        answer: "option1"
+    },
+    {
+        title: "this is question 2",
+        options: ["option1", "options2", "option3"],
+        answer: "option3"
+    },
+    {
+        title: "this is question 3",
+        options: ["option1", "options2", "option3"],
+        answer: "option2"
+    },
+    {
+        title: "this is question 4",
+        options: ["option1", "options2", "option3"],
+        answer: "option2"
+    }
+]
+console.log(questions[1].title);
